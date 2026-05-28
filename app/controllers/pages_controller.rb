b042fab5573f31dae12fc36572581ad358bdfa6d
vraic <288215058+vraic@users.noreply.github.com>
2026-05-28 18:23:56 +0100

    add tailwindcss-rails and authentication

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  allow_unauthenticated_access only: :home
+
+  def home
+    redirect_to dashboard_path if authenticated?
+  end
+
+  def dashboard
+    redirect_to new_session_path unless authenticated?
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "pages#home"
+  get "dashboard" => "pages#dashboard"
 end
