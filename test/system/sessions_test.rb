6994d496779619c6a7778fc5e896c6c31cf39a39
vraic <288215058+vraic@users.noreply.github.com>
2026-05-28 18:57:32 +0100

    #4 fix typo in route path

--- test/system/sessions_test.rb
+++ test/system/sessions_test.rb
@@ -1,8 +1,8 @@
 require "application_system_test_case"
 
 class SessionsTest < ApplicationSystemTestCase
-  test "visiting new sessions / login page" do
-    visit new_sessions_url
+  test "visiting new sessions page" do
+    visit new_session_url
 
     assert_selector "h1", text: "Sign in to your account"
   end
